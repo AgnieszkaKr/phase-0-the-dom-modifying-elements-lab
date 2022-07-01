c60d6e287cd7b302e9ffb57f2f7618186d423c9c
Agnes <agsawicka@gmail.com>
2022-07-01 13:28:16 -0400

    commit tree c7ac3ee78980c43af6b1431436b3898952c7202f

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const mainRemove = document.querySelector('main')
+mainRemove.remove()
+
+let newHeader = document.createElement('h1')
+// const header = newHeader.setAttribute('#victory')
+newHeader.setAttribute("id", "victory")
+newHeader.textContent = 'Me is the champion ..'
+document.body.append(newHeader)
+
+
+// document.querySelector(newHeader).textContent = 'Agnes is the champion"';
